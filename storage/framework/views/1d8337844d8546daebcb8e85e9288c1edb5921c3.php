f85e0da1b9538e3db0b18c6c83de35a4b7bc8af0
Francy <francy@trueplus.vn>
2018-03-20 11:11:19 +0700

    git cache cleared

--- storage/framework/views/1d8337844d8546daebcb8e85e9288c1edb5921c3.php
+++
@@ -1,3 +0,0 @@
-<hr>
-
-<p class="text-center">Coppyright Jacurtis - All Rights Reserved - Thank you my Teacher</p>
--- storage/framework/views/8fc4597aba2e38de7dcba9d4a8c83bf0273232bc.php
+++
@@ -1,1 +0,0 @@
-<h5 class="text-center">Coppyright Jacurtis - All Rights Reserved - Thank you my Teacher</h5>
